70c81d533e8cb5cd033ff324018eaf704f811cf4
Ian Dees <undees@gmail.com>
2010-12-20 23:40:08 -0800

    Plain NodeMixin is way faster

--- jruby-degrees/models.rb
+++ jruby-degrees/models.rb
@@ -3,15 +3,31 @@ require 'neo4j'
 
 Neo4j::Config[:storage_path] = 'movies'
 
-class Movie < Neo4j::Model; end
+module Finders
+  def find_by_name(name)
+    (found = find %Q[name: "#{name}"]) && found.first
+  end
 
-class Actor < Neo4j::Model
+  def find_or_create_by_name(name)
+    find_by_name(name) || new(:name => name)
+  end
+end
+
+class Movie; end
+
+class Actor
+  include Neo4j::NodeMixin
+  extend Finders
+
   property :name
   index :name
   has_n(:movies).to(Movie)
 end
 
-class Movie < Neo4j::Model
+class Movie
+  include Neo4j::NodeMixin
+  extend Finders
+
   property :name
   index :name
   has_n(:actors).from(Actor, :movies)
