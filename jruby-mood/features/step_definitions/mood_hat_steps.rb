c8182b57c38f1a76fb2cb6f229dbce93ab5d323e
Ian Dees <undees@gmail.com>
2011-03-15 13:30:12 -0700

    Simpler cuke world

--- jruby-mood/features/step_definitions/mood_hat_steps.rb
+++ jruby-mood/features/step_definitions/mood_hat_steps.rb
@@ -1,13 +1,13 @@
 Given /^I am (.+)$/ do |mood|
-  $port.putc int_for(mood).to_s
+  @port.putc int_for(mood).to_s
 end
 
 When /^I ([^ ]+) my happiness$/ do |change|
   char = (change == 'increase') ? '+' : '-'
-  $port.putc char
+  @port.putc char
 end
 
 Then /^I should be (.+)$/ do |mood|
-  $port.putc '?'
-  $port.getc.to_i.should == int_for(mood)
+  @port.putc '?'
+  @port.getc.to_i.should == int_for(mood)
 end
--- jruby-mood/features/support/env.rb
+++ jruby-mood/features/support/env.rb
@@ -1,18 +1,21 @@
 $: << File.dirname(__FILE__) + '/../../lib'
 require 'serialport'
 
-module MoodHelper
+class MoodWorld
   Emotions = %w(furious unhappy neutral happy ecstatic)
+
+  def initialize(port)
+    @port = port
+  end
 
   def int_for(emotion)
     Emotions.index emotion
   end
 end
 
-$port = SerialPort.new ENV['MOOD_HAT'], 9600, 8, 1, SerialPort::NONE
-$port.read_timeout = 1000
-$port.putc('?') until ($port.getc =~ /\d/ rescue nil)
+port = SerialPort.new ENV['MOOD_HAT'], 9600, 8, 1, SerialPort::NONE
+port.read_timeout = 1000
+port.putc('?') until (port.getc =~ /\d/ rescue nil)
+at_exit { port.close }
 
-World MoodHelper
-
-at_exit { $port.close }
+World { MoodWorld.new(port) }
